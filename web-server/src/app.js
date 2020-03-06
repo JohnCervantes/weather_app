5089a35aec692b7356ab576eb2324870e1e11643
fletcher <fletcher@localhost.localdomain>
2020-03-05 22:17:52 -0800

    version 1.0

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,8 +6,8 @@ const app = express();
 const root = path.join(__dirname, "../");
 const viewsPath = path.join(__dirname + "/../../templates/views");
 const partialssPath = path.join(__dirname + "/../../templates/partials");
-const url = "https://api.darksky.net/forecast/74bbdabdc35d2ebfc273693155571716/37.8267,-122.4233?units=si";
-const geocodingUrl =  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoidm9jYWxpc3RzIiwiYSI6ImNrN2Y3eHpwMjAwNGEzZXA5eTA2MjZmY2MifQ.IvX-KOOEWTTIQ6n8xfGzEA";
+const geocode = require("../utils/geocode");
+const forecast = require("../utils/forecast");
 
 // console.log(root+ 'templates/views');
 // console.log();
@@ -17,19 +17,29 @@ app.set("views", viewsPath);
 hbs.registerPartials(partialssPath);
 //app.use(express.static(root));
 
-
-request({url:url, json:true},(error, response)=>{
-  if(error){
-    return res.send('Unable to connect to the weather service');
-  }
-  console.log(response.body.daily.data[0].summary);
-});
-
 app.get("", (req, res) => {
-  if(req.query.address){
-    return res.render('index',{title: "Homepage", address: req.query.address});
+  if (!req.query.address) {
+    return res.render("",{
+      error: "You must provide an address!"
+    });
   }
-  res.render("index", { title: "Homepage" });
+
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.render("",{
+        forecast: `Current Forecast: ${forecastData}`,
+        loc : `Location:  ${location}`
+      });
+    });
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -39,13 +49,13 @@ app.get("/about", (req, res) => {
   });
 });
 
-app.get('/about/*',(req,res)=>{
-  res.send('About article Not Found');
-  })
+app.get("/about/*", (req, res) => {
+  res.send("About article Not Found");
+});
 
-app.get('*',(req,res)=>{
-res.render('error');
-})
+app.get("*", (req, res) => {
+  res.render("error");
+});
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
